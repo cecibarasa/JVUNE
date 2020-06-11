c24557898d87d3e3058f7e24c1ebd3cb3fc49391
Cecilia Barasa <cecibarasa@gmail.com>
2020-06-11 08:38:23 +0300

    register blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -15,4 +15,8 @@ def create_app(config_name):
     # Initializing Flask Extensions
     bootstrap.init_app(app)
 
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
     return app
