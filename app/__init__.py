6b4d0f513f6a896514a4224ad41f90fb2c5d8de4
Cecilia Barasa <cecibarasa@gmail.com>
2020-06-11 08:36:19 +0300

    add config options

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,18 @@
 from flask import Flask
-from .config import DevConfig
+from config import config_options
 from flask_bootstrap import Bootstrap
 
-#Initializing the application
-app = Flask(__name__,instance_relative_config=True)
+bootstrap = Bootstrap()
 
-#set configuration
-app.config.from_object(DevConfig)
+def create_app(config_name):
 
-# Initializing Flask Extensions
-bootstrap = Bootstrap(app)
+    #Initializing the application
+    app = Flask(__name__)
 
+    #set configuration
+    app.config.from_object(config_options[config_name])
 
-from app import views
+    # Initializing Flask Extensions
+    bootstrap.init_app(app)
+
+    return app
--- config.py
+++ config.py
@@ -26,3 +26,8 @@ class DevConfig(Config):
     '''
 
     DEBUG = True
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig
+}    
