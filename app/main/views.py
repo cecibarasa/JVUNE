53b3a33a8965b380157ae22ce2aba27b1dd8f1f4
Cecilia Barasa <cecibarasa@gmail.com>
2020-06-13 13:50:31 +0300

    add user route

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
 from flask import render_template, request, redirect,url_for
 from . import main
 from ..requests import get_quote
+from flask_login import login_required
 
 #views
 @main.route('/')
@@ -11,4 +12,15 @@ def index():
     title = 'JVUNE | Welcome to JVUNE Blogs'
     quote = get_quote()
 
-    return render_template('index.html', title = title, quote = quote)
+    return render_template('index.html', title=title, quote=quote)
+    
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    title = f"{uname.capitalize()}'s Profile"
+
+    if user is None:
+        abort(404)
+
+    return render_template('profile/profile.html',user = user, title=title)    
