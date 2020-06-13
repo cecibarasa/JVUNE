a0806872e47f70039beeeacca4394724b37b4b72
Cecilia Barasa <cecibarasa@gmail.com>
2020-06-13 13:55:46 +0300

    add usermixin

--- app/main/views.py
+++ app/main/views.py
@@ -23,4 +23,22 @@ def profile(uname):
     if user is None:
         abort(404)
 
-    return render_template('profile/profile.html',user = user, title=title)    
+    return render_template('profile/profile.html', user=user, title=title)
+
+@main.route('/user/<uname>/update', methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname = user.username))
+
+    return render_template('profile/update.html', form = form)       
--- app/models.py
+++ app/models.py
@@ -1,17 +1,20 @@
 from . import db
-from werkzeug.security import generate_password_hash,check_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 class Quote:
 
     def __init__(self, author, quote):
         self.author = author
         self.quote = quote
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255))
 
-    pass_secure  = db.Column(db.String(255))
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    password_hash = db.Column(db.String(255))
 
         @property
         def password(self):
