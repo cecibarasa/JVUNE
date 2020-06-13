ac7d16b638c94f3c5148028108fd1144c38da954
Cecilia Barasa <cecibarasa@gmail.com>
2020-06-13 12:54:06 +0300

    add manger.shell

--- app/models.py
+++ app/models.py
@@ -1,5 +1,14 @@
+from . import db
 class Quote:
 
     def __init__(self, author, quote):
         self.author = author
         self.quote = quote
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'User {self.username}'        
