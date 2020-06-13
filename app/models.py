36747e434c39ab77cf7f4ba31c7948ad08a89619
Cecilia Barasa <cecibarasa@gmail.com>
2020-06-13 13:57:51 +0300

    add registraion form

--- app/models.py
+++ app/models.py
@@ -1,6 +1,11 @@
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 class Quote:
 
     def __init__(self, author, quote):
