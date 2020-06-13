d4465a830ce7484bd3b9890c76b59be4ec1cead6
Cecilia Barasa <cecibarasa@gmail.com>
2020-06-13 12:45:45 +0300

    add postgress

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, request, redirect,url_for
 from . import main
+from ..requests import get_quote
 
 #views
 @main.route('/')
@@ -7,8 +8,7 @@ def index():
     '''
     view root page that retunrs index page with its data
     '''
-    title = 'JVUNE - Welcome to JVUNE Blogs'
+    title = 'JVUNE | Welcome to JVUNE Blogs'
+    quote = get_quote()
 
-    return render_template('index.html', title = title)
-
-   
+    return render_template('index.html', title = title, quote = quote)
