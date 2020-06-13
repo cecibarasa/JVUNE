0552b74d610dc0504f1cbb343fa94afc9d561159
Cecilia Barasa <cecibarasa@gmail.com>
2020-06-13 16:37:05 +0300

    add flask uploads

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,10 @@
 from flask import render_template, request, redirect,url_for
 from . import main
 from ..requests import get_quote
-from flask_login import login_required
-from .forms import ReviewForm,UpdateProfile
+from flask_login import login_required,current_user
+from .forms import UpdateProfile,BlogForm,CommentForm
 from .. import db
+from ..models import User,Blog,Comment
 
 #views
 @main.route('/')
@@ -27,7 +28,7 @@ def profile(uname):
 
     return render_template('profile/profile.html', user=user, title=title)
 
-@main.route('/user/<uname>/update', methods = ['GET','POST'])
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
 @login_required
 def update_profile(uname):
     user = User.query.filter_by(username = uname).first()
@@ -38,9 +39,106 @@ def update_profile(uname):
 
     if form.validate_on_submit():
         user.bio = form.bio.data
+
         db.session.add(user)
         db.session.commit()
 
-        return redirect(url_for('.profile',uname = user.username))
+        return redirect(url_for('.profile',uname=user.username))
 
-    return render_template('profile/update.html', form = form)       
+    return render_template('profile/update.html', form=form)
+
+@main.route('/user/<uname>/update/pic', methods = ['POST'])
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+
+    return redirect(url_for('main.profile', uname=uname))
+
+@main.route('/blog/delete/<int:id>', methods = ['GET', 'POST'])
+@login_required
+def delete_blog(id):
+    blog = Blog.get_blog(id)
+    db.session.delete(blog)
+    db.session.commit()
+
+    return render_template('blogs.html', id=id, blog=blog)
+
+@main.route('/blogs/new', methods = ['GET','POST'])
+@login_required
+def new_blog():
+    legend = 'New Blog'
+    form = BlogForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        blog = form.text.data
+        category = form.category.data
+
+        new_blog = Blog(blog_title = title,blog_content = blog, category = category,user = current_user)
+        new_blog.save_blog()
+
+        return redirect(url_for('main.index'))
+
+    title = 'New Blog'
+    return render_template('new_blog.html', legend=legend, title=title, blog_form=form)
+
+@main.route('/blog/comment/delete/<int:id>', methods = ['GET', 'POST'])
+@login_required
+def delete_comment(id):
+    comment = Comment.query.filter_by(id=id).first()
+    blog_id = comment.blog
+    Comment.delete_comment(id)
+
+    return redirect(url_for('main.blog',id=blog_id))
+
+@main.route('/blog/<int:id>', methods = ["GET","POST"])
+def blog(id):
+    blog = Blog.get_blog(id)
+    posted_date = blog.posted.strftime('%b %d, %Y')
+
+    form = CommentForm()
+    if form.validate_on_submit():
+        comment = form.text.data
+        name = form.name.data
+
+        new_comment = Comment(comment = comment, name = name, blog_id = blog)
+
+        new_comment.save_comment()
+
+    comments = Comment.get_comments(blog)
+
+    return render_template('blog.html', blog = blog, comment_form = form,comments = comments, date = posted_date)
+
+@main.route('/user/<uname>/blogs', methods = ['GET','POST'])
+def user_blogs(uname):
+    user = User.query.filter_by(username = uname).first()
+    blogs = Blog.query.filter_by(user_id = user.id).all()
+
+    return render_template('profile/blogs.html', user = user, blogs = blogs)
+
+@main.route('/blogs/recent', methods = ['GET','POST'])
+def blogs():
+    blogs = Blog.query.order_by(Blog.id.desc()).limit(5)
+
+    return render_template('blogs.html',blogs = blogs)
+
+@main.route('/blog/<int:id>/update', methods = ['GET','POST'])
+@login_required
+def update_blog(id):
+    legend = 'Update Blog'
+    blog = Blog.get_blog(id)
+    form = BlogForm()
+    if form.validate_on_submit():
+        blog.blog_title = form.title.data
+        blog.blog_content = form.text.data
+        
+        db.session.commit()
+        return redirect(url_for('main.blog', id = id))
+    elif request.method == 'GET':
+        form.title.data = blog.blog_title
+        form.text.data = blog.blog_content
+    form.category.data = blog.category
+    return render_template('new_blog.html', legend = legend, blog_form = form, id=id)                     
--- app/models.py
+++ app/models.py
@@ -15,13 +15,12 @@ class Quote:
         self.quote = quote
 
 class User(UserMixin,db.Model):
-    class User(UserMixin,db.Model):
+    
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(20), index = True)
     email = db.Column(db.String(255),unique = True, index = True)
     bio = db.Column(db.String(1000))
-    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
     profile_pic_path = db.Column(db.String)
     secure_password = db.Column(db.String(255),nullable = False)
     blogs = db.relationship('Blog',backref = 'user', lazy = 'dynamic')
