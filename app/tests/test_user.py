24f97db27307303ecfebf16e50b124a870dce103
Cecilia Barasa <cecibarasa@gmail.com>
2020-06-13 13:42:07 +0300

    add blueprint

--- app/tests/test_user.py
+++ app/tests/test_user.py
@@ -8,3 +8,10 @@ class UserModelTest(unittest.TestCase):
 
     def test_password_setter(self):
         self.assertTrue(self.new_user.pass_secure is not None)
+
+    def test_no_access_password(self):
+            with self.assertRaises(AttributeError):
+                self.new_user.password
+
+        def test_password_verification(self):
+            self.assertTrue(self.new_user.verify_password('banana'))    
