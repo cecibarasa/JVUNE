2fecfa25c24ca0fc2671e62cee210d2848794e45
Cecilia Barasa <cecibarasa@gmail.com>
2020-06-15 16:41:21 +0300

    move start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-export SECRET_KEY='1738'
-export MAIL_USERNAME='ceciheroku@gmail.com'
-export MAIL_PASSWORD='barasa2015'
-export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://nabalayo:karitie@localhost/jvune'
-
-python3.6 manage.py server
